a465889ff544734123d238fa7168c9161a7803fa
Aspectious <griffin.siebert14@outlook.com>
2023-04-14 09:10:52 -0400

    gfjngksdnfgjfg

--- src/game.js
+++ src/game.js
@@ -5,11 +5,13 @@ export class Game {
     constructor() {
         this.board = document.getElementById("board").getContext("2d");
         this.DrawGridLines()
+        this.BlockList = new Array<Array>(20);
     }
     DrawGridLines() {
         for(var r = 0; r!=20; r++) {
             for(var c=0; c!=10; c++) {
-                this.board.rect((40*c),(40*r),40,40)
+
+                this.board.rect((20*c),(20*r),20,20)
                 this.board.stroke();
             };
         }
